9d23770d4a731f79433cd2ba516fc2a30826a5c8
Hancock <courtneyehanco1k-dce@ssrb-vpn1-5-189.vpn.ufl.edu>
2020-05-19 16:33:01 -0400

    Edit main content

--- index.php
+++ index.php
@@ -1,29 +1,30 @@
 <?php get_header(); ?>
 
-<div class="container">
-  <div class="row">
-  <?php
-    if(have_posts()){
-      while(have_posts()){
-        the_post();?>
+<div class="main-content">
+  <div class="container">
+    <div class="row">
+    <?php
+      if(have_posts()){
+        while(have_posts()){
+          the_post();?>
 
-        <div class="col-lg-4 posts">
-          <?php the_post_thumbnail('medium'); ?>
-          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-          <p class="category-label">Category: <?php the_category(); ?></p>
-          <?php the_excerpt(); ?>
-          <?php
-            $archive_year = get_the_time('Y');
-            $archive_month = get_the_time('m');
-            $archive_day = get_the_time('d');
-          ?>
-          <p>Published: <a href="<?php echo get_day_link($archive_year, $archive_month, $archive_day); ?>"><?php echo get_the_date(); ?></a></p>
-          <p><?php echo "Article written by: " . get_the_author(); ?></p>
-        </div>
-  <?php } //ends while loop
-    }//end if statement
-    ?>
+          <div class="col-lg-4 posts">
+            <?php the_post_thumbnail('medium'); ?>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <p class="category-label">Category: <?php the_category(); ?></p>
+            <?php the_excerpt(); ?>
+            <?php
+              $archive_year = get_the_time('Y');
+              $archive_month = get_the_time('m');
+              $archive_day = get_the_time('d');
+            ?>
+            <p>Published: <a href="<?php echo get_day_link($archive_year, $archive_month, $archive_day); ?>"><?php echo get_the_date(); ?></a></p>
+            <p><?php echo "Article written by: " . get_the_author(); ?></p>
+          </div>
+    <?php } //ends while loop
+      }//end if statement
+      ?>
+    </div>
   </div>
 </div>
-
 <?php get_footer(); ?>
