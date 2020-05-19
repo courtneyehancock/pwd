e9053d8d8872b9f7314a902dc7b182d2b61e7140
Hancock <courtneyehanco1k-dce@ssrb-vpn1-5-189.vpn.ufl.edu>
2020-05-19 16:44:59 -0400

    Add italic class to published, category and written by

--- index.php
+++ index.php
@@ -17,9 +17,9 @@
               $archive_month = get_the_time('m');
               $archive_day = get_the_time('d');
             ?>
-            <p>Published: <a href="<?php echo get_day_link($archive_year, $archive_month, $archive_day); ?>"><?php echo get_the_date(); ?></a></p>
-            <p class="category-label">Category: <?php the_category(); ?></p>
-            <p><?php echo "Article written by: " . get_the_author(); ?></p>
+            <p class="font-italic">Published: <a href="<?php echo get_day_link($archive_year, $archive_month, $archive_day); ?>"><?php echo get_the_date(); ?></a></p>
+            <p class="category-label font-italic">Category: <?php the_category(); ?></p>
+            <p class="font-italic"><?php echo "Article written by: " . get_the_author(); ?></p>
           </div>
     <?php } //ends while loop
       }//end if statement
