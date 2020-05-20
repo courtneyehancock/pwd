bf9c8a6c1bbce9d718ced66f45b293831bdeef6b
Hancock <courtneyehanco1k-dce@ssrb-vpn2-10-44.vpn.ufl.edu>
2020-05-20 12:05:41 -0400

    Add title to blog page

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 
 <div class="main-content">
+  <div class="container-fluid">
+    <h1 style="color: #00529b; font-size: 2em; text-align: right; margin-right: 2em;">OPWD INSIDER</h1>
+  </div>
   <div class="container">
     <div class="row">
     <?php
