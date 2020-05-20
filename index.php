f062380eb2914ac69e0fa35421eb14b19ce9fd23
Hancock <courtneyehanco1k-dce@ssrb-vpn2-10-44.vpn.ufl.edu>
2020-05-20 12:24:04 -0400

    Edit box shadow, columns for posts

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
     </div>
   </div>
   <div class="container">
-    <div class="row">
+    <div class="row justify-content-center">
     <?php
       if(have_posts()){
         while(have_posts()){
           the_post();?>
 
-          <div class="col-lg-4 posts">
+          <div class="col-lg-5 posts">
             <?php the_post_thumbnail('medium'); ?>
             <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
             <?php the_excerpt(); ?>
