68e19f868851264ded292eb9aaeabfe62587a11e
Hancock <courtneyehanco1k-dce@ssrb-vpn1-2-191.vpn.ufl.edu>
2020-06-01 10:11:05 -0400

    Edit header configuration

--- header.php
+++ header.php
@@ -14,23 +14,27 @@
 <header>
   <div class="align-content-center">
     <div class="row main-nav">
-      <div class="col-md-1" id="site-logo">
-        <!--If/else for Logo and Site Title-->
-        <?php if(get_header_image() == '') { ?>
-          <h1><a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></h1>
-          <?php
-        }else{?>
-          <a href="<?php echo home_url('/'); ?>"><img class="align-middle uf-logo" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Logo" /></a>
-          <?php
-        }
-        ?>
+      <div class="col-md-5" id="site-logo">
+          <div>
+          <!--If/else for Logo and Site Title-->
+          <?php if(get_header_image() == '') { ?>
+            <h1><a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></h1>
+            <?php
+          }else{?>
+            <a href="<?php echo home_url('/'); ?>"><img class="align-middle uf-logo" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Logo" /></a>
+            <?php
+          }
+          ?>
+        </div>
       </div>
-      <div class="col-md-4">
-        <!--Navigation-->
-        <a href="<?php echo home_url('/'); ?>"><div class="header-text">
-          <p class="head-text">Professional and Workforce Development</p>
-        </div></a>
-      </div>
+
+        <div>
+          <!--Navigation-->
+          <a href="<?php echo home_url('/'); ?>"><div class="header-text">
+            <p class="head-text">Professional and Workforce Development</p>
+          </div></a>
+        </div>
+
       <div class="col-md-7" id="site-nav">
         <!--Navigation-->
         <?php wp_nav_menu(array(
