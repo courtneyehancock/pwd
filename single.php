d73d5db566996728bc8b7d7a592b089d380e856a
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-04-02 10:20:33 -0400

    Add hero image to single post pages

--- single.php
+++ single.php
@@ -1,6 +1,12 @@
 <?php get_header(); ?>
 
 <div class="main-content pt-5 pb-5">
+  <div class="hero-image" style="background-color:#001b3d; background-image: linear-gradient(to bottom, rgba(0, 27, 61, 1), rgba(245, 246, 252, 0.52)), url('https://pwd.aa.ufl.edu/wp-content/uploads/2021/03/0I1A5562-scaled.jpg'); height:300px; background-position: 50% 25%; background-size:cover;">
+<div class="container internal-div">
+<h1 class="hero-text" style="margin-top:80px;">OPWD INSIDER</h1>
+<div class="block"></div>
+</div>
+</div>
   <div class="container">
   <?php
     if(have_posts()){
