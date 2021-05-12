84a5636eceed6588712e7c76fc738f4517b425ec
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-05-12 12:35:14 -0400

    Edit 404 page

--- 404.php
+++ 404.php
@@ -1,14 +1,15 @@
 <?php /*Template Name: 404 Page*/?>
 <?php get_header(); ?>
-
-<div class="container">
-<div class="row">
-  <div class="col-lg-12 not-found">
-    <h2>Page Not Found</h2>
-    <h3>This usually never happens.</h3>
-    <p>It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a></p>
+<div class="main-content">
+<div class="internal-hero-image" style="background-color:#001b3d; background-image: linear-gradient(to bottom, rgba(0, 27, 61, 1), rgba(245, 246, 252, 0.52)), url('https://pwd.aa.ufl.edu/wp-content/uploads/2021/03/0I1A5562-scaled.jpg');">
+</div>
+<div class="container mt-5 mb-5 text-center">
+  <div class="not-found">
+    <img src="https://pwd.aa.ufl.edu/wp-content/uploads/2021/05/Telescope-Icon-404.png" class="img-fluid" style="max-width:300px;"/>
+    <h1 class="mt-4">OOPS! 404 NOT FOUND</h1>
+    <h2>This usually never happens.</h2>
+    <p class="pb-5">It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a>?</p>
   </div>
 </div>
 </div>
-
 <?php get_footer(); ?>
