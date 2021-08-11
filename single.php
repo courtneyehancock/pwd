6a58f42b5465b1d204631bb111f302616fb99eaf
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-08-11 14:21:54 -0400

    Edit blog layout

--- single.php
+++ single.php
@@ -20,9 +20,15 @@
             ?>
             <p class="written-by mt-3"><?php echo "Written by: " . get_the_author(); ?></p>
             <p class="published"><?php echo "Published: " . get_the_date(); ?></p>
+            <hr>
+            <p class="pl-2 pr-2"><em>Keep up with all things OPWD by reading our bi-monthly office updates.</em></p>
+
+            <p class="pl-2 pr-2"><em>The Office of Professional and Workforce Development (OPWD) supports the Office of Teaching and Technology and the University of Florida in offering non-credit education for adult learners across multiple modalities, e.g. online, face-to-face, and conferences.</em></p>
+
+            <p class="pl-2 pr-2"><em>Is there a topic under our umbrella that you would like to learn or see more about? Email Courtney Hancock at <a href="mailto:cehancock@mail.ufl.edu">cehancock@mail.ufl.edu</a> with your ideas.</em></p>
           </div>
           <div class="col-md-9 p-4">
-            <div class="post-feature">
+            <div class="post-feature shadow">
               <?php the_post_thumbnail(); ?>
             </div>
             <h1 class="post-title mt-3"><?php the_title(); ?></h1>
