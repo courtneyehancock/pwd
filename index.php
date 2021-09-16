992bd2666d902ef8a1b6081acf02bf9cb1ee3577
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-09-16 12:53:50 -0400

    Change link color for post categories and remove unnecessary styling from old post styles

--- index.php
+++ index.php
@@ -35,26 +35,12 @@
           <div class="block"></div>
           <div class="post-info">
             <p class="font-italic">Published: <?php echo get_the_date(); ?></p>
-        <!--<p class="category-label font-italic">Category: <?php the_category(); ?></p>-->
           </div>
           <p></p></div>
           <p>    <a href="<?php the_permalink(); ?>" class="btn btn-primary card-btn">READ POST</a>
           </p></div>
           </div>
-      <!--  <div class="col-lg-3 posts p-3">
-            <?php the_post_thumbnail('medium'); ?>
-            <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <?php the_excerpt(); ?>
-            <?php
-              $archive_year = get_the_time('Y');
-              $archive_month = get_the_time('m');
-              $archive_day = get_the_time('d');
-            ?>
-            <div class="post-info">
-              <p class="font-italic">Published: <?php echo get_the_date(); ?></p>-->
-            <!--  <p class="category-label font-italic">Category: <?php the_category(); ?></p>
-            </div>
-          </div>-->
+
     <?php } //ends while loop
       }//end if statement
       ?>
