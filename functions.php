5ec11733df65eaa9f5bbd7fe1768900f7a7cec0d
Glancy <courtneyehanco1k@TSS-OPWD-001A.local>
2022-12-06 14:03:01 -0500

    Take away font awesome stylesheet linked in functions folder

--- functions.php
+++ functions.php
@@ -16,9 +16,6 @@
   //main CSS
     wp_enqueue_style('main-styles', get_stylesheet_uri());
 
-  //font awesome
-  	wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/css/all.css');
-
   //Javascript files
     wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/main.js');
     wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js');
@@ -40,7 +37,7 @@
       'before_title'  => '<h3 class="widget-title">',
       'after_title'   => '</h3>'
     ));
-    
+
     //Header: Social Widget
     register_sidebar(array(
       'name'          => ('Head Social Widget'),
