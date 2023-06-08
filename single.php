7c1fd97885c05a21891b2cb8152a7fecd806ee6d
courtneyehancock <courtneyehanco1k@TSS-OPWD-001A.local>
2023-06-08 07:39:50 -0400

    Edit hero image margins of blog php

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="main-content pt-5 pb-5">
-  <div class="internal-hero-image" style="background-color:#001b3d; background-image: linear-gradient(360deg, #00529be3, #00285ddb, #001a3d), url('https://pwd.aa.ufl.edu/wp-content/uploads/2021/03/0I1A5562-scaled.jpg');">
+  <div class="internal-hero-image-blog" style="background-color:#001b3d; background-image: linear-gradient(360deg, #00529be3, #00285ddb, #001a3d), url('https://pwd.aa.ufl.edu/wp-content/uploads/2021/03/0I1A5562-scaled.jpg');">
 <div class="container internal-div">
 <h1 class="internal-hero-text">OPWD NEWS</h1>
 </div>
