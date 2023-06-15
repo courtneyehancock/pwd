a9256ab96ebff196b1642282587124febf4423a0
courtneyehancock <courtneyehanco1k@TSS-OPWD-001A.local>
2023-06-15 14:32:45 -0400

    Edit the php template for the footer columns

--- footer.php
+++ footer.php
@@ -2,19 +2,15 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-4">
-        <!--Logo/text widget-->
         <?php dynamic_sidebar('left-footer'); ?>
       </div>
       <div class="col-lg-2">
-        <!--Middle Footer-->
         <?php dynamic_sidebar('middle-footer'); ?>
       </div>
       <div class="col-lg-3">
-        <!--Middle Footer-->
         <?php dynamic_sidebar('middle-right-footer'); ?>
       </div>
       <div class="col-lg-3">
-        <!--Middle Footer-->
         <?php dynamic_sidebar('right-footer'); ?>
       </div>
     </div>
