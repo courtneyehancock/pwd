f54948aec68189153832068ba2cad1409172e1f7
courtneyehancock <courtneyehanco1k@TSS-OPWD-001A.local>
2023-10-30 11:46:08 -0400

    Copy new code to archive

--- archive.php
+++ archive.php
@@ -4,7 +4,7 @@ get_header();
 <div class="main-content">
   <div class="internal-hero-image" style="background-color:#001b3d; background-image: linear-gradient(360deg, #00529be3, #00285ddb, #001a3d), url('https://pwd.aa.ufl.edu/wp-content/uploads/2021/03/0I1A5562-scaled.jpg');">
   <div class="container internal-div">
-  <h1 class="internal-hero-text text-center archive-title">Category: <?php
+  <h1 class="internal-hero-text text-center archive-title">Search Results: <?php
       if(is_category()){
         single_cat_title();
       }elseif(is_tag()){
@@ -23,35 +23,44 @@ get_header();
 
   </div>
   </div>
-  <div class="container mt-5 mb-5 pb-5">
-        <div class="row justify-content-center align-items-start">
+  <div class="container pt-5 pb-5">
+    <div class="row">
+      <div class="col-lg-9">
+    <div class="row justify-content-center align-items-start">
+    <?php
+      if(have_posts()){
 
-        <?php if(have_posts()){
-                while(have_posts()){
-                  the_post(); ?>
-                  <div class="col-xxl-2 m-3">
-                  <div class="card shadow">
-                    <img class="card-img-top" <?php the_post_thumbnail('medium'); ?><br>
-                  <img class="card-img-featured" <?php
-                    echo get_avatar( get_the_author_email(), '60' );
-                  ?> <br>
-                  <span class="badge badge-pill badge-primary spec"><?php the_category(); ?></span>
-                  <div class="card-body">
-                  <h3 class="post-title card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                  <div class="block"></div>
-                  <div class="post-info">
-                    <p class="font-italic">Published: <?php echo get_the_date(); ?></p>
-                <!--<p class="category-label font-italic">Category: <?php the_category(); ?></p>-->
-                  </div>
-                  <p></p></div>
-                  <p>    <a href="<?php the_permalink(); ?>" class="btn btn-primary card-btn">READ POST</a>
-                  </p></div>
-                  </div>
-              <?php  }
-              }
-        ?>
-      </div>
-</div>
-</div>
+        while(have_posts()){
+          the_post();?>
+          <div class="col-xxl-2 m-3">
+          <div class="card shadow">
+            <img class="card-img-top" <?php the_post_thumbnail('medium'); ?>
+            <div class="post-info">
+              <p><?php echo get_the_author();?><p>
+              <p><?php echo get_the_date(); ?></p>
+            </div>
 
-<?php get_footer(); ?>
+          <div class="card-body">
+          <h3 class="post-title card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+          <span class="blog-excerpt"><?php the_excerpt(); ?></span>
+          <div class="card-line"></div>
+          <span class="blog-category pt-2"><?php the_category(); ?></span>
+          </div>
+        </div>
+          </div>
+
+    <?php
+  } //ends while loop
+      }//end if statement
+      ?>
+    </div>
+  </div>
+    <div class="col-lg-3 p-3">
+      <!--Recent Posts-->
+      <?php dynamic_sidebar('recent-posts'); ?>
+
+    </div>
+  </div>
+  </div>
+  </div>
+  <?php get_footer(); ?>
