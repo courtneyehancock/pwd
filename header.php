99b4e569982485f97122b81362dc0aaedf6b8b00
courtneyehancock <courtneyehanco1k@TSS-OPWD-001A.local>
2024-06-10 09:01:24 -0400

    Remove CDN bootsrap from header.php

--- header.php
+++ header.php
@@ -14,10 +14,6 @@
   <!--Facebook Meta Tag-->
   <meta name="facebook-domain-verification" content="rtkjhpxmfz6ny9m5emxv1v6j2up7jk" />
 
-  <!--Boostrap Styles & JS-->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
   <!--Classica Web Font-->
   <link rel="stylesheet" href="https://use.typekit.net/roz5znz.css">
   <!--font awesome-->
